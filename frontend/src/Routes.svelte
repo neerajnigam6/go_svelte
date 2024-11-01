cfb6581f2e5272f144c4ba12fb42f585830d3564
Ubuntu <neerajnigam6@gmail.com>
2024-11-01 07:55:25 +0000

    fit: sync

---
+++ frontend/src/Routes.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Router from 'svelte-spa-router'
+    import Video from './lib/Screens/Video.svelte';
+
+    let routes = {
+        '/video/:id': Video
+    }
+</script>
+
+<Router {routes} />
